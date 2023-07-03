d4a827127e9c0d54076b748bcaa4b31698938ed2
Francis Marasigan <francis.marasigan@gmail.com>
2023-07-03 15:38:04 -0700

    Update run.sh

--- nx584/run.sh
+++ nx584/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/with-contenv bashio
-python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect 192.168.86.82:10000 --config /usr/app/src/config.ini
+python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect usr-tcp232-302.local:10000 --config /usr/app/src/config.ini
