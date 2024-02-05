52d412a8808597ca6c20232e36904e4a1bcbae0f
heatvent <matteocp@gmail.com>
2024-02-05 10:49:24 -0600

    Update run.sh

--- nx584/run.sh
+++ nx584/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/with-contenv bashio
-python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect 192.168.68.125:10000 --config /usr/app/src/config.ini
+python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect 192.168.2.131:10000 --config /usr/app/src/config.ini
